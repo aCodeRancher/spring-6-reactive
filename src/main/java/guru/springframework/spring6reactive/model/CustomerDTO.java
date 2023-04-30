8c771036b3d93231edc229840d30c64e87f93600
helen <hhma@alumni.cpp.edu>
2023-04-30 17:59:20 -0400

    add creation date and last modified date.

--- src/main/java/guru/springframework/spring6reactive/model/CustomerDTO.java
+++ src/main/java/guru/springframework/spring6reactive/model/CustomerDTO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,4 +14,6 @@ import lombok.NoArgsConstructor;
 public class CustomerDTO {
     private Integer id;
     private String customerName;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
 }
