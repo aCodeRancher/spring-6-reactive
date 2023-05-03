c65892c3c7c7ecb4ee7b95004740a9afd5bfc29e
helen <hhma@alumni.cpp.edu>
2023-05-02 22:09:00 -0400

    add patch test

--- src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
@@ -54,6 +54,15 @@ class BeerControllerTest {
     }
 
     @Test
+    void testPatchBeerNotFound() {
+        webTestClient.patch()
+                .uri(BeerController.BEER_PATH_ID, 999)
+                .body(Mono.just(BeerRepositoryTest.getTestBeer()), BeerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
     @Order(3)
     void testUpdateBeer() {
         webTestClient.put()
